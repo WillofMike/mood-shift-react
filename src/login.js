9a07fe0cb9272a2f11b200c2044420cfd7b76dad
Mike <you@example.com>
2018-01-23 16:01:33 -0700

    Added images to the dashboard and slightly styled the sign in and login page

--- src/login.js
+++ src/login.js
@@ -1,23 +1,75 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background: lightgrey;
+  height: 100%;
+  display: grid;
+  grid-gap: 3px;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: 100px;
+  grid-template-areas:
+      "h . . . ."
+      ". o . . ."
+      ". e p . ."
+      ". b . . ."
+      ". . f . .";
+  header {
+    font-size: 26px;
+    grid-area: h;
+  }
+  h1 {
+    font-size: 16px;
+    font-weight: bold;
+    grid-area: o;
+  }
+  button {
+    font-size: 1em;
+    border-radius: 3px;
+    width: auto;
+    grid-area: b;
+  }
+  #email {
+    height: 20px;
+    width: auto;
+    grid-area: e;
+  }
+  #password {
+    height: 20px;
+    width: auto;
+    grid-area: p;
+  }
+  footer {
+    grid-area: f;
+    padding-top: 200px;
+  }
+`;
+
+
 
 const Form = (props) =>
-  <div>
-    <div>{props.title}</div>
+  <Wrapper className="container">
+    <header>Mood Shift</header>
+    <h1 className="header">{props.title}</h1>
       <input
+        id="email"
         placeholder="Email"
         type="text"
         value={props.email}
         onChange={props.setEmail}
       />
       <input
+        id="password"
         placeholder="Password"
         type="password"
         value={props.password}
         onChange={props.setPassword}
       />
-    <button onClick={props.submit}>Submit</button>
-  </div>;
+    <br/>
+  <button onClick={props.submit}>Submit</button>
+  <footer>Copyright Mood-Shift</footer>
+</Wrapper>;
 
 class Login extends React.Component {
   constructor() {
